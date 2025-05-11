43eb39611ceffe655f8b909b0300b080461ca6ff
Matheus Morilha <matheusmorilha04@gmail.com>
2025-05-11 16:56:24 -0300

    Ajuste no nome do arquivo user_register para singUpScreen

--- lib/widgets/login/login_links.dart
+++ lib/widgets/login/login_links.dart
@@ -1,4 +1,4 @@
-import 'package:adotai/screens/user_register.dart';
+import 'package:adotai/screens/singUpScreen.dart';
 import 'package:flutter/material.dart';
 
 class SingInEmailLink extends StatelessWidget {
